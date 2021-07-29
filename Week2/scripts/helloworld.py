d2efb9b7ffc05ae5f4a91f369a037167c98da607
Parallax Knight <84042346+ParallaxKnight@users.noreply.github.com>
2021-07-29 19:40:53 +0530

    Update helloworld.py

--- Week2/scripts/helloworld.py
+++ Week2/scripts/helloworld.py
@@ -4,25 +4,31 @@ from std_msgs.msg import String
 
 class HelloWorld:
     def __init__(self):
-        self.pub = rospy.Publisher('helloworld', String, queue_size=1)
+        self.pub = rospy.Publisher('helloworld2', String, queue_size=1)
         self.sub1 = rospy.Subscriber('hello', String, self.callback1)
         self.sub2 = rospy.Subscriber('world', String, self.callback2)
-        self.str = String()
-        self.hello = None
-        self.world = None
+        self.message = String()
+        self.hello = String()
+        self.world = String()
+        
     
     def callback1(self,msg):
         self.hello = msg.data
-        self.str = str(self.hello) + ', ' + str(self.world) + '!'
-        self.pub.publish(self.str)
+
 
     def callback2(self,msg):
         self.world = msg.data
-        self.str = str(self.hello) + ', ' + str(self.world) + '!'
-        self.pub.publish(self.str)
+        self.printer()
+
+
+    def printer(self):
+        self.message = str(self.hello) + ', ' + str(self.world) + '!'
+        if 'data' not in self.message:
+            self.pub.publish(self.message)
+            print(self.message)
         
 
 if __name__ == "__main__":
-    rospy.init_node('helloworld')
+    rospy.init_node('helloworld2')
     name = HelloWorld()
     rospy.spin()
